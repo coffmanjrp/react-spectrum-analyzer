cd8301425dcd5410a88af3688e292d059a7831c7
coffmanjrp <coffmanjrp@gmail.com>
2021-11-02 16:47:39 -0700

    Add the player function

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ function App() {
 
   const audioCtxRef = useRef(null);
   const [source, setSource] = useState(null);
-  const [analyserNode, setAnalyserNode] = useState(null);
+  const [analyzerNode, setAnalyzerNode] = useState(null);
+
+  const [playState, setPlayState] = useState('stop');
+  const [duration, setDuration] = useState(0);
+  const [timePosition, setTimePosition] = useState(0);
 
   useEffect(() => {
     audioCtxRef.current = new AudioContext();
@@ -20,18 +24,74 @@ function App() {
 
     elementSource.connect(analyzer).connect(audioCtxRef.current.destination);
     setSource(elementSource);
-    setAnalyserNode(analyzer);
+    setAnalyzerNode(analyzer);
   }, []);
+
+  const handleTogglePlay = () => {
+    if (audioCtxRef.current.state === 'suspended') {
+      audioCtxRef.current.resume();
+      setPlayState('play');
+    }
+
+    if (playState === 'stop') {
+      audioRef.current.play();
+      setPlayState('play');
+    }
+
+    if (playState === 'play') {
+      audioRef.current.pause();
+      setPlayState('stop');
+    }
+  };
+
+  const handleTimeUpdate = () => {
+    setTimePosition(audioRef.current.currentTime);
+  };
+
+  const handleChangeTimePosition = (e) => {
+    const position = parseInt(e.target.value);
+
+    setTimePosition(position);
+    audioRef.current.currentTime = position;
+  };
+
+  const handleEnded = () => {
+    setTimePosition(0);
+    setPlayState('stop');
+  };
+
+  const handleLoadedMetadata = () => {
+    const duration = audioRef.current.duration;
+
+    setDuration(duration);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <button type="button">Stop</button>
-        <input type="range" ref={timePositionRef} />
+        <button type="button" onClick={handleTogglePlay}>
+          {playState === 'stop' && 'play'}
+          {playState === 'play' && 'stop'}
+        </button>
 
-        <audio src="./audio/audio.m4a" ref={audioRef} />
+        <input
+          type="range"
+          ref={timePositionRef}
+          min={0}
+          max={duration}
+          value={timePosition}
+          onInput={handleChangeTimePosition}
+        />
+
+        <audio
+          src="./audio/audio.m4a"
+          ref={audioRef}
+          onTimeUpdate={handleTimeUpdate}
+          onLoadedMetadata={handleLoadedMetadata}
+          onEnded={handleEnded}
+        />
 
         <canvas className="spectrums" ref={spectrumRef} />
       </header>
