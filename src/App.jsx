a6326477bfdb57fd632075e55249e24b6ff1cbe7
coffmanjrp <coffmanjrp@gmail.com>
2021-11-02 16:24:30 -0700

    Place the elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
+import { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const audioRef = useRef(null);
+  const timePositionRef = useRef(null);
+  const spectrumRef = useRef(null);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button type="button">Stop</button>
+        <input type="range" ref={timePositionRef} />
+
+        <audio src="./audio/audio.m4a" ref={audioRef} />
+
+        <canvas className="spectrums" ref={spectrumRef} />
       </header>
     </div>
   );
